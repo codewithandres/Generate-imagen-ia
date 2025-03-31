209a4359d3983e8b9978c2f766aae5d815cfa024
Dize dev <axiozweb2022@gmail.com>
2025-03-31 12:14:53 -0500

    Agregar funcionalidad de cambio de tema con almacenamiento local y estilos para el tema oscuro

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,33 @@
 import './style.css';
 import './css/toggle-theme.css';
 
-console.log('script');
+const Themetoggle = document.querySelector('.toggle-theme') as HTMLElement;
+
+(() => {
+    const saveTheme = localStorage.getItem('theme');
+
+    const systemPreferenceDark = window.matchMedia(
+        '(prefers-color-scheme: dark)'
+    ).matches;
+
+    const isDarkTheme =
+        saveTheme === 'dark ' || (!saveTheme && systemPreferenceDark);
+
+    document.body.classList.toggle('dark-theme', isDarkTheme);
+
+    Themetoggle.querySelector('i')!.className = isDarkTheme
+        ? 'ri-sun-foggy-fill'
+        : 'ri-moon-clear-line';
+})();
+
+const changeTheme = () => {
+    const isDarkTheme = document.body.classList.toggle('dark-theme');
+
+    localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
+
+    Themetoggle.querySelector('i')!.className = isDarkTheme
+        ? 'ri-sun-foggy-fill'
+        : 'ri-moon-clear-line';
+};
+
+Themetoggle.addEventListener('click', changeTheme);
